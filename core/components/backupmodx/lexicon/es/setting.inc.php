7840fefa8a13c0da49d7098ab7bbc1f9c87a389e
Thomas Jakobi <buero@treehillstudio.de>
2025-11-17 12:43:13 +0100

    Added translation using Weblate (Spanish)

---
+++ core/components/backupmodx/lexicon/es/setting.inc.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Setting Lexicon Entries for BackupMODX
+ *
+ * @package backupmodx
+ * @subpackage lexicon
+ */
+$_lang['area_cron'] = '';
+$_lang['setting_backupmodx.cronDatabase'] = '';
+$_lang['setting_backupmodx.cronDatabase_desc'] = '';
+$_lang['setting_backupmodx.cronEnable'] = '';
+$_lang['setting_backupmodx.cronEnable_desc'] = '';
+$_lang['setting_backupmodx.cronFiles'] = '';
+$_lang['setting_backupmodx.cronFiles_desc'] = '';
+$_lang['setting_backupmodx.cronKey'] = '';
+$_lang['setting_backupmodx.cronKey_desc'] = '';
+$_lang['setting_backupmodx.cronMaxDatabase'] = '';
+$_lang['setting_backupmodx.cronMaxDatabase_desc'] = '';
+$_lang['setting_backupmodx.cronMaxFiles'] = '';
+$_lang['setting_backupmodx.cronMaxFiles_desc'] = '';
+$_lang['setting_backupmodx.cronNote'] = '';
+$_lang['setting_backupmodx.cronNote_desc'] = '';
+$_lang['setting_backupmodx.debug'] = '';
+$_lang['setting_backupmodx.debug_desc'] = '';
+$_lang['setting_backupmodx.excludeFiles'] = '';
+$_lang['setting_backupmodx.excludeFiles_desc'] = '';
+$_lang['setting_backupmodx.excludeFolders'] = '';
+$_lang['setting_backupmodx.excludeFolders_desc'] = '';
+$_lang['setting_backupmodx.groups'] = '';
+$_lang['setting_backupmodx.groups_desc'] = '';
+$_lang['setting_backupmodx.targetPath'] = '';
+$_lang['setting_backupmodx.targetPath_desc'] = '';
+$_lang['setting_backupmodx.timelimit'] = '';
+$_lang['setting_backupmodx.timelimit_desc'] = '';
