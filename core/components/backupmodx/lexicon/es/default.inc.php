c01bd15e86218b1fcb81de94e9f5981be62bb0c0
Thomas Jakobi <buero@treehillstudio.de>
2025-11-17 12:43:02 +0100

    Added translation using Weblate (Spanish)

---
+++ core/components/backupmodx/lexicon/es/default.inc.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Default Lexicon Entries for BackupMODX
+ *
+ * @package backupmodx
+ * @subpackage lexicon
+ */
+$_lang['backupmodx'] = '';
+$_lang['backupmodx.widget'] = '';
+$_lang['backupmodx.widget_desc'] = '';
+$_lang['backupmodx.add_readme'] = '';
+$_lang['backupmodx.btn_cancel'] = '';
+$_lang['backupmodx.btn_remove'] = '';
+$_lang['backupmodx.btn_restore'] = '';
+$_lang['backupmodx.btn_start'] = '';
+$_lang['backupmodx.database'] = '';
+$_lang['backupmodx.download'] = '';
+$_lang['backupmodx.files'] = '';
+$_lang['backupmodx.intro'] = '';
+$_lang['backupmodx.notes'] = '';
+$_lang['backupmodx.restore'] = '';
+$_lang['backupmodx.err_backup_ne'] = '';
+$_lang['backupmodx.err_create_backup'] = '';
+$_lang['backupmodx.err_create_target_path'] = '';
+$_lang['backupmodx.err_missing_backup_options'] = '';
+$_lang['backupmodx.err_missing_restore_options'] = '';
+$_lang['backupmodx.err_msg_title'] = '';
+$_lang['backupmodx.err_no_backups_available'] = '';
+$_lang['backupmodx.err_no_restore_available'] = '';
+$_lang['backupmodx.err_pdo_exeption'] = '';
+$_lang['backupmodx.err_timeout'] = '';
+$_lang['backupmodx.err_unknown'] = '';
+$_lang['backupmodx.success_msg_title'] = '';
+$_lang['backupmodx.success_restore'] = '';
+$_lang['backupmodx.msg_finished'] = '';
+$_lang['backupmodx.msg_progress_text'] = '';
+$_lang['backupmodx.msg_progress_title'] = '';
